cda79911f221c330df45d02a0864410165ac0aa2
Steven Rondeau <steve.rondeau@sentinelspc.com>
2018-04-16 14:34:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var name = 'George'
 function happyHolidays() {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+console.log(happyHolidaysTo(name))
